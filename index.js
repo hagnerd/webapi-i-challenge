0ad290a29c67632a8fa4ed95db38e3923bdb0477
Matt Hagner <web@matthagner.com>
2019-08-07 18:16:54 -0500

    Basic server set up
    - Import and instantiate express
    - use express.json() for parsing the body of requests
    - Set up server to listen on port 4000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require("express");
+
+const db = require("./data/db");
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(4000, () => {
+  console.log(`The server is listening on port 4000`);
+});
