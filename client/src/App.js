1f25ab906fa8b78c9053867acc1761a301084de6
Matt Hagner <web@matthagner.com>
2019-08-07 20:06:17 -0500

    Add Routes for application

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { useFetch } from "./hooks";
+
+import Home from "./pages/home";
+import NewUserForm from "./pages/new-user-form";
+import EditUserForm from "./pages/edit-user-form";
 
 function App() {
+  const { data, loading, error, refetch } = useFetch({
+    url: "http://localhost:4000/api/users"
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div>Hello World</div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/new">New</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route
+          path="/new"
+          render={props => <NewUserForm refetch={refetch} {...props} />}
+        />
+        <Route
+          path="/edit/:id"
+          render={props => (
+            <EditUserForm
+              users={data ? data.users : []}
+              refetch={refetch}
+              {...props}
+            />
+          )}
+        />
+        <Route
+          path="/"
+          render={() => (
+            <Home users={data ? data.users : []} refetch={refetch} />
+          )}
+        />
+      </Switch>
+    </>
   );
 }
 
